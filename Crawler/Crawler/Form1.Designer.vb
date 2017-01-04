991287ed72994aabde6fbcf703b6dd2fd4b4bbad
Arash Hatami <hatamiarash7@gmail.com>
2017-01-04 18:33:08 +0330

    Add

---
+++ Crawler/Crawler/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.جدیدToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ذخیرهToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.بازیابیToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.خروجToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.تمامصفحهToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.تغییرتمToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ThemeWhite = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ThemeBlack = New System.Windows.Forms.ToolStripMenuItem()
+        Me.مدیریتToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.لیستسایتهاToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.پایگاهدادهToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.پشتیبانگیریToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.بازیابیToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.دربارهToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.توضیحاتToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.وبسایتToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.ViewToolStripMenuItem, Me.مدیریتToolStripMenuItem, Me.پایگاهدادهToolStripMenuItem, Me.دربارهToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1084, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.جدیدToolStripMenuItem, Me.ذخیرهToolStripMenuItem, Me.بازیابیToolStripMenuItem, Me.خروجToolStripMenuItem})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(40, 20)
+        Me.ToolStripMenuItem1.Text = "فایل"
+        '
+        'جدیدToolStripMenuItem
+        '
+        Me.جدیدToolStripMenuItem.Name = "جدیدToolStripMenuItem"
+        Me.جدیدToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.جدیدToolStripMenuItem.Text = "جدید"
+        '
+        'ذخیرهToolStripMenuItem
+        '
+        Me.ذخیرهToolStripMenuItem.Name = "ذخیرهToolStripMenuItem"
+        Me.ذخیرهToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.ذخیرهToolStripMenuItem.Text = "ذخیره"
+        '
+        'بازیابیToolStripMenuItem
+        '
+        Me.بازیابیToolStripMenuItem.Name = "بازیابیToolStripMenuItem"
+        Me.بازیابیToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.بازیابیToolStripMenuItem.Text = "بازیابی"
+        '
+        'خروجToolStripMenuItem
+        '
+        Me.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem"
+        Me.خروجToolStripMenuItem.Size = New System.Drawing.Size(108, 22)
+        Me.خروجToolStripMenuItem.Text = "خروج"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.تمامصفحهToolStripMenuItem, Me.تغییرتمToolStripMenuItem})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.ViewToolStripMenuItem.Text = "ظاهر"
+        '
+        'تمامصفحهToolStripMenuItem
+        '
+        Me.تمامصفحهToolStripMenuItem.Name = "تمامصفحهToolStripMenuItem"
+        Me.تمامصفحهToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
+        Me.تمامصفحهToolStripMenuItem.Text = "تمام صفحه"
+        '
+        'تغییرتمToolStripMenuItem
+        '
+        Me.تغییرتمToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ThemeWhite, Me.ThemeBlack})
+        Me.تغییرتمToolStripMenuItem.Name = "تغییرتمToolStripMenuItem"
+        Me.تغییرتمToolStripMenuItem.Size = New System.Drawing.Size(129, 22)
+        Me.تغییرتمToolStripMenuItem.Text = "تغییر تم"
+        '
+        'ThemeWhite
+        '
+        Me.ThemeWhite.Name = "ThemeWhite"
+        Me.ThemeWhite.Size = New System.Drawing.Size(107, 22)
+        Me.ThemeWhite.Text = "سفید"
+        '
+        'ThemeBlack
+        '
+        Me.ThemeBlack.Name = "ThemeBlack"
+        Me.ThemeBlack.Size = New System.Drawing.Size(107, 22)
+        Me.ThemeBlack.Text = "مشکی"
+        '
+        'مدیریتToolStripMenuItem
+        '
+        Me.مدیریتToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.لیستسایتهاToolStripMenuItem})
+        Me.مدیریتToolStripMenuItem.Name = "مدیریتToolStripMenuItem"
+        Me.مدیریتToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.مدیریتToolStripMenuItem.Text = "مدیریت"
+        '
+        'لیستسایتهاToolStripMenuItem
+        '
+        Me.لیستسایتهاToolStripMenuItem.Name = "لیستسایتهاToolStripMenuItem"
+        Me.لیستسایتهاToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.لیستسایتهاToolStripMenuItem.Text = "لیست سایت ها"
+        '
+        'پایگاهدادهToolStripMenuItem
+        '
+        Me.پایگاهدادهToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.پشتیبانگیریToolStripMenuItem, Me.بازیابیToolStripMenuItem1})
+        Me.پایگاهدادهToolStripMenuItem.Name = "پایگاهدادهToolStripMenuItem"
+        Me.پایگاهدادهToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
+        Me.پایگاهدادهToolStripMenuItem.Text = "پایگاه داده"
+        '
+        'پشتیبانگیریToolStripMenuItem
+        '
+        Me.پشتیبانگیریToolStripMenuItem.Name = "پشتیبانگیریToolStripMenuItem"
+        Me.پشتیبانگیریToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.پشتیبانگیریToolStripMenuItem.Text = "پشتیبان گیری"
+        '
+        'بازیابیToolStripMenuItem1
+        '
+        Me.بازیابیToolStripMenuItem1.Name = "بازیابیToolStripMenuItem1"
+        Me.بازیابیToolStripMenuItem1.Size = New System.Drawing.Size(137, 22)
+        Me.بازیابیToolStripMenuItem1.Text = "بازیابی"
+        '
+        'دربارهToolStripMenuItem
+        '
+        Me.دربارهToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.توضیحاتToolStripMenuItem, Me.وبسایتToolStripMenuItem})
+        Me.دربارهToolStripMenuItem.Name = "دربارهToolStripMenuItem"
+        Me.دربارهToolStripMenuItem.Size = New System.Drawing.Size(47, 20)
+        Me.دربارهToolStripMenuItem.Text = "درباره"
+        '
+        'توضیحاتToolStripMenuItem
+        '
+        Me.توضیحاتToolStripMenuItem.Name = "توضیحاتToolStripMenuItem"
+        Me.توضیحاتToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.توضیحاتToolStripMenuItem.Text = "توضیحات"
+        '
+        'وبسایتToolStripMenuItem
+        '
+        Me.وبسایتToolStripMenuItem.Name = "وبسایتToolStripMenuItem"
+        Me.وبسایتToolStripMenuItem.Size = New System.Drawing.Size(122, 22)
+        Me.وبسایتToolStripMenuItem.Text = "وب سایت"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1084, 611)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Main"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents جدیدToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ذخیرهToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents بازیابیToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents خروجToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents تمامصفحهToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents تغییرتمToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ThemeWhite As ToolStripMenuItem
+    Friend WithEvents ThemeBlack As ToolStripMenuItem
+    Friend WithEvents پایگاهدادهToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents پشتیبانگیریToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents بازیابیToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents دربارهToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents توضیحاتToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents وبسایتToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents مدیریتToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents لیستسایتهاToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Crawler/Crawler/Form2.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SiteManager
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.TextBox1.Location = New System.Drawing.Point(98, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(332, 31)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label1.Location = New System.Drawing.Point(436, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "آدرس سایت : "
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.Button1.Location = New System.Drawing.Point(12, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 33)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "ثبت"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 23
+        Me.ListBox1.Location = New System.Drawing.Point(12, 43)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(524, 280)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.Button2.Location = New System.Drawing.Point(13, 336)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 32)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "ویرایش"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.Button3.Location = New System.Drawing.Point(108, 336)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 32)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "حذف"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Calibri", 14.25!)
+        Me.Button4.Location = New System.Drawing.Point(422, 336)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(114, 32)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "ثبت در پایگاه"
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SiteManager
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 380)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "SiteManager"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
